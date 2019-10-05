06a04c709d88a43e6dae1f093e6e36b494670492
Luiz Carlos Aguiar <luizcarlosaguiarc@gmail.com>
2019-10-05 19:40:52 -0300

    Imprime o menu de login na main

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pyrebase
 import curses
 import time
+import menu
 
-menu_login = ('Login', 'Registrar', 'Sair')
+def main(stdscr):
+    menu_login = ('Login', 'Registrar', 'Sair')
 
+    curses.curs_set(0)
+    current_row_idx = 0
+
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
+
+    menu.print_menu(stdscr, current_row_idx, menu_login)
+
+    stdscr.refresh()
+
+    stdscr.getch()
+
+curses.wrapper(main)
