33955b3f3fc6e982ce7670f39770f9be3a202a69
Luiz Aguiar <themax285gamer@gmail.com>
2019-10-05 21:20:28 -0300

    Centraliza os textos de login

--- login.py
+++ login.py
@@ -12,17 +12,28 @@ def start_login(stdscr):
         "appId": "1:581051665954:web:6f131448200a100689447b"
     }
 
+    altura_tela, largura_tela = stdscr.getmaxyx()
+
     # Faz conexao com Firebase
     firebase = pyrebase.initialize_app(config)
 
-    stdscr.addstr(0,0,"Nome: ")
-    stdscr.addstr(1,0, "Senha: ")
+    name_label = "Nome: "
+    pass_label = "Senha: "
+
+    x_nome = largura_tela//2 - 15
+    y_nome = altura_tela//2
+
+    x_senha = largura_tela//2 - 15
+    y_senha = altura_tela//2 + 1
+
+    stdscr.addstr(y_nome, x_nome, name_label)
+    stdscr.addstr(y_senha, x_senha, pass_label)
 
     curses.echo()
-    user_name = stdscr.getstr(0,len("Nome: "),15)
+    user_name = stdscr.getstr(y_nome,x_nome + len(name_label),15)
 
     curses.echo(False)
-    user_password = stdscr.getstr(1,len("Senha: "),15)
+    user_password = stdscr.getstr(y_senha,x_senha + len(pass_label),15)
 
     stdscr.getch()
 
