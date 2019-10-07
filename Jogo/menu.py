25748ddf30b25dbd4b5537e8d9dc58a9b03a6764
Luiz Aguiar <themax285gamer@gmail.com>
2019-10-06 22:17:06 -0300

    Adiciona comentarios no menu.py

--- Jogo/menu.py
+++ Jogo/menu.py
@@ -1,13 +1,22 @@
 import curses
-def print_menu(stdscr, selected_row_idx, current_menu):
-    stdscr.clear()
-    altura, largura = stdscr.getmaxyx()
 
+# Funcao que cria menus
+def print_menu(stdscr, selected_row_idx, current_menu):
+    # Limpa a Tela
+    stdscr.clear()
+
+    # Altura e Largura da Tela
+    altura, largura = stdscr.getmaxyx()
+    
+    # Pega o indice e o texto do item atual do menu
     for indice, row in enumerate(current_menu):
         text = row
+        
+        # Coordenadas do texto
         x = largura//2 - len(text)//2
         y = altura//2 - len(current_menu)//2 + indice
-
+        
+        # Colore parte selecionada diferente das demais
         if indice == selected_row_idx:
             stdscr.attron(curses.color_pair(2))
             stdscr.addstr(y, x, row)
