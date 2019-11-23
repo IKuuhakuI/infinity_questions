7500dccc106ae7ebbe0a2e9f7ac510abdb702234
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-23 03:58:00 -0300

    Cria a funcao back_btn

--- Jogo/menu.py
+++ Jogo/menu.py
@@ -25,3 +25,29 @@ def print_menu(stdscr, selected_row_idx, current_menu):
             stdscr.addstr(y, x, row)
     
     stdscr.refresh()
+
+# Funcao que cria botao voltar
+def back_btn(stdscr, selected_row_idx, current_menu):
+    # Limpa a Tela
+    stdscr.clear()
+
+    # Altura e Largura da Tela
+    altura, largura = stdscr.getmaxyx()
+    
+    # Pega o indice e o texto do item atual do menu
+    for indice, row in enumerate(current_menu):
+        text = row
+        
+        # Coordenadas do texto
+        x = largura//2 - len(text)//2
+        y = altura - (altura//8) - len(current_menu)//2 + indice
+        
+        # Colore parte selecionada diferente das demais
+        if indice == selected_row_idx:
+            stdscr.attron(curses.color_pair(2))
+            stdscr.addstr(y, x, row)
+            stdscr.attron(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, row)
+    
+    stdscr.refresh()
