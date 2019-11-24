e9cc24ed6a9c62c89759142dc84b80a6bd7b5d77
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-23 23:01:41 -0300

    Cria funcao de menu horizontal

--- Jogo/menu.py
+++ Jogo/menu.py
@@ -51,3 +51,30 @@ def back_btn(stdscr, selected_row_idx, current_menu):
             stdscr.addstr(y, x, row)
     
     stdscr.refresh()
+
+def horizontal_menu(stdscr, selected_row_idx, current_menu):
+    # Altura e Largura da Tela
+    altura, largura = stdscr.getmaxyx()
+
+    total_menu_len = 0
+
+    for indice,row in enumerate(current_menu):
+        total_menu_len += len(row)
+    
+    # Pega o indice e o texto do item atual do menu
+    for indice, row in enumerate(current_menu):
+        text = row
+        
+        # Coordenadas do texto
+        x = largura//2 - total_menu_len//2 - len(row) + total_menu_len * indice
+        y = altura - altura//8
+        
+        # Colore parte selecionada diferente das demais
+        if indice == selected_row_idx:
+            stdscr.attron(curses.color_pair(2))
+            stdscr.addstr(y, x, row)
+            stdscr.attron(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, row)
+    
+    stdscr.refresh()
