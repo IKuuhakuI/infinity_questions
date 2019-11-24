3133099a8604477703794cd46a6a7eb20342c3fa
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-24 16:44:07 -0300

    Adiciona acoes do teclado

--- Jogo/actions.py
+++ Jogo/actions.py
@@ -12,3 +12,19 @@ def verify_next(entrada):
 	if entreada == "/next":
 		return True
 	return False
+
+def keyboard_actions(entrada):
+    if entrada == curses.KEY_UP:
+        return 'up'
+
+    elif entrada == curses.KEY_DOWN:
+        return 'down'
+
+    elif entrada == curses.KEY_RIGHT:
+        return 'right'
+
+    elif entrada == curses.KEY_LEFT:
+        return 'left'
+
+    elif entrada == curses.KEY_ENTER or entrada in [10,13]:
+        return 'enter'
