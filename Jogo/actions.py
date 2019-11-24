45127f451c58c46b1a840bcb088d9c7544f9ffe2
Yuri <yuripdantas@poli.ufrj.br>
2019-11-24 16:18:10 -0300

    Adicionando funcoes no arquivo actions.py

--- Jogo/actions.py
+++ Jogo/actions.py
@@ -2,3 +2,13 @@ def verify_exit(entrada):
     if entrada == "/exit":
         return True
     return False
+
+def verify_back(entrada):
+	if entrada ==  "/back":
+		return True
+	return False
+
+def verify_next(entrada):
+	if entreada == "/next":
+		return True
+	return False
