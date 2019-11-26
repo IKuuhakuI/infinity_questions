75f3109aa23de1f148458a1b82e9492b6a9a6457
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-26 18:43:03 -0300

    Corrige erro de digitacao do show_rule_screen

--- Jogo/screen.py
+++ Jogo/screen.py
@@ -21,7 +21,7 @@ def show_title_screen(stdscr):
 			break
 
 # Tela que vem logo apos o usuario clicar em 'jogar'
-def show_rules_screem(stdscr):
+def show_rules_screen(stdscr):
 	lista_texto_rules = ['Bem Vindo ao Infinity Questions!','O objetivo é ver quem consegue acertar o maximo de perguntas sem errar!','Você acha que consegue quebrar o recorde?','Para continuar, aperte qualquer coisa...']
 	linhas = 4
 	textPrint.print_multi_lines(stdscr, lista_texto_rules, linhas)
