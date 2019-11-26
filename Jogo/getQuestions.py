1ddba1a1c77691a6691d15ff7876c50a42009e70
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-25 21:25:59 -0300

    Aleatoriza as perguntas para o jogo

---
+++ Jogo/getQuestions.py
@@ -0,0 +1,48 @@
+import pyrebase
+import random
+
+def random_questions(question_list):
+    random.shuffle(question_list)
+    return question_list
+
+def create_question_list(question_list):
+    final_list = []
+    for idx in range(len(question_list)):
+        dict_quest = {"Id":idx + 1,
+                      "Pergunta":question_list[idx]["Pergunta"]}
+        final_list.append(dict_quest)
+
+    return final_list
+
+def get_questions_data():
+    config = {
+        "apiKey": "AIzaSyBrarBhWJSP3FnNJurEAtrbmUb1fG_wZFs",
+        "authDomain": "teste-python-67d43.firebaseapp.com",
+        "databaseURL": "https://teste-python-67d43.firebaseio.com",
+        "projectId": "teste-python-67d43",
+        "storageBucket": "",
+        "messagingSenderId": "581051665954",
+        "appId": "1:581051665954:web:6f131448200a100689447b"
+    }
+
+    firebase = pyrebase.initialize_app(config)
+
+    db_questions = firebase.database()
+
+    question_list = db_questions.child('Perguntas').get().val()
+
+    if question_list[0] == None:
+        question_list.pop(0)
+
+    final_list = create_question_list(question_list)
+
+    print(final_list)
+
+    print()
+
+    final_list = random_questions(final_list)
+
+    return final_list
+
+print(get_questions_data())
+
