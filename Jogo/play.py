2c225a1dfb76f0165fdfb64db25b14a1996d9da9
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-25 21:49:48 -0300

    Testa pergunta aleatoria na play.py

--- Jogo/play.py
+++ Jogo/play.py
@@ -1,8 +1,34 @@
 # Criando arquivo play.py
 #arquivo que vai ficar a logica do jogo propriamente dito
 #Aqui dentro vai ser chamado as telas de recorde pessoal, global ou não recorde
-import screen 
+import curses
 
-def final_game (stdscr):
-	screen.show_welcome_screen(stdscr)
-	
+import screen
+import textPrint 
+import getQuestions
+
+def final_game(stdscr):
+    # Imprime titulo do jogo
+    textPrint.print_title(stdscr)
+    # Imprime regras do jogo
+    screen.show_welcome_screen(stdscr)
+    stdscr.clear()
+
+    # Imprime titulo do jogo
+    textPrint.print_title(stdscr)
+    # Tela de carregamento
+    textPrint.print_center(stdscr, "Carregando...")
+    stdscr.refresh()
+
+    # Lista com as perguntas do Jogo
+    lista_perguntas = getQuestions.get_questions_data()
+    stdscr.clear()
+
+    hasGivenUp = False
+    hasLost = False
+
+    textPrint.print_center(stdscr, lista_perguntas[0]["Pergunta"])
+
+    stdscr.getch()
+
+curses.wrapper(final_game)
