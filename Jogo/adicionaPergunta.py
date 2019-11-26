1a69e681294fc691b06774d3caa529c331755cfa
Luiz Aguiar <themax285gamer@gmail.com>
2019-11-26 18:32:51 -0300

    Limpa a tela antes de imprimir regras

--- Jogo/adicionaPergunta.py
+++ Jogo/adicionaPergunta.py
@@ -12,6 +12,8 @@ import actions
 def adiciona_pergunta(stdscr, current_user_id, current_user_data):
 	continuar_voltar_menu = ('Continuar', 'Voltar')
 
+	stdscr.clear()
+
 	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
 
